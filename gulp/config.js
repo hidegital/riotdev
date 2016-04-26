8f81f00c846148a1e5f2b22ab6d0bc38bf62eb09
山内英夫 <wiz_yamauchi@yamauchihideo-no-MacBook-Pro.local>
2016-04-26 16:43:08 +0900

    taskを分ける

---
+++ gulp/config.js
@@ -0,0 +1,14 @@
+const paths = {
+	js:         ['src/js/**/*.js', 'src/js/**/*.jade'],
+	jade:       'src/jade/*.jade',
+	jade_watch: 'src/jade/**/*.jade',
+	sass:       'src/sass/*.sass',
+	sass_watch: 'src/sass/**/*.sass',
+	dist:       'dist/**/*',
+	dist_dir:   'dist',
+	js_dir:     'dist/js',
+	sass_dir:   'src/sass',
+	css_dir:    'dist/css'
+};
+
+export default paths;
---
+++ gulp/tasks/sass.js
@@ -0,0 +1,14 @@
+import gulp    from 'gulp';
+import sass    from 'gulp-sass';
+import notify  from 'gulp-notify';
+import plumber from 'gulp-plumber';
+import paths   from '../config';
+
+gulp.task('sass', () => {
+  return gulp.src(paths.sass)
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>')
+    }))
+    .pipe(sass())
+    .pipe(gulp.dest(paths.css_dir));
+});
